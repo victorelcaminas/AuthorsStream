fd0ee1c6e509ddf55d471286fe86c3f90a3d1b64
victor <victor@ieselcaminas.org>
2022-12-01 19:20:49 +0100

    Initial commit

---
+++ src/Author.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Author {
+    private String name;
+    private String email;
+    private char gender;
+    private int year;
+
+    public Author(String name, String email, char gender, int year) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+        this.year = year;
+    }
+
+    public static Author readFromKeyboard() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String name = input.nextLine();
+        System.out.print("Email: ");
+        String email = input.nextLine();
+        System.out.print("Enter gender (M/F): ");
+        char gender = input.next().toUpperCase().charAt(0);
+        System.out.print("Birth year: ");
+        int year = input.nextInt();
+
+        return new Author(name, email, gender, year);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return name + "(" + gender + ") at " + email;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Author a = Author.readFromKeyboard();
+        System.out.println(a);
+
+
+
+    }
+}
