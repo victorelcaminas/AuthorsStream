7a7643720a29c9de35c1dafdd6751c0d6508d3d3
victor <victor@ieselcaminas.org>
2022-12-01 20:29:28 +0100

    update 2

--- src/WriteObjectAuthors.java
+++ src/WriteObjectAuthors.java
@@ -1,18 +1,24 @@
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.util.Scanner;
 
 public class WriteObjectAuthors {
     public static void main(String[] args) throws IOException {
         ObjectOutputStream out = null;
         try {
-            new ObjectOutputStream((
+            out = new ObjectOutputStream((
                     new BufferedOutputStream(
                             new FileOutputStream("authorsobject.dat")
                     )
             ));
-
+            String answer = "y";
+            Scanner input = new Scanner(System.in);
+            while(!answer.equals("n")) {
+                System.out.println("Enter Author");
+                Author a = Author.readFromKeyboard();
+                out.writeObject(a);
+                System.out.println("Another author? (Y/N):");
+                answer = input.next().toLowerCase();
+            }
         } finally {
             if (out != null) {
                 out.close();
